cca44ac3224ce2466c1a13713ec8591ef8d04288
akahard2dj <akahard2dj@naver.com>
2018-04-23 14:15:54 +0900

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,9 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
+    
+    from .api.v1 import api as api_1_0_blueprint
+    app.register_blueprint(api_1_0_blueprint, url_prefix='/api/v1')
 
     db.init_app(app)
 
