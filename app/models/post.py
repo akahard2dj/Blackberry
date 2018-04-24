3a59d01fea087db256bdb83521a339e4e6e044e1
akahard2dj <akahard2dj@naver.com>
2018-04-24 14:02:04 +0900

    Create post.py

---
+++ app/models/post.py
@@ -0,0 +1,8 @@
+from app import db
+
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text)
+    body = db.Column(db.Text)
