6e4715c61539a28ae2028a21c328458fbf27d9b5
akahard2dj <akahard2dj@naver.com>
2018-04-24 17:32:07 +0900

    Update __init__.py

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,5 +1,9 @@
 from flask import Blueprint
+from flask_restful import Api
 
-api = Blueprint('api', __name__)
+from app.api.v1_0.test_api import TestApi
 
-from . import test_api
+api_bp = Blueprint('api', __name__)
+api = Api(api_bp)
+
+api.add_resource(TestApi, '/testapi')
