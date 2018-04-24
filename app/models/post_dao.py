cb43ff00ba28ff700ca3e55f8d5d42ffda07c44d
akahard2dj <akahard2dj@naver.com>
2018-04-24 16:26:42 +0900

    Update post_dao.py

--- app/models/post_dao.py
+++ app/models/post_dao.py
@@ -1,20 +1,31 @@
+import json
+
 from app.models.post import Post
 from app.models.post_dto import PostDTO
 
 from app import db
 
+
 class PostDAO(object):
     def __init__(self):
         self.__post = None
 
-    def get_posts(self):
+    def get_posts_obj(self):
         q = Post.query.all()
         posts = list()
         for p in q:
-            post_dto = PostDTO()
-            post_dto.id = p.id
-            post_dto.title = p.title
-            post_dto.body = p.body
+            post_dto = PostDTO(p)
             posts.append(post_dto)
 
         return posts
+
+    def get_posts_dict(self):
+        q = Post.query.all()
+        posts = list()
+        for p in q:
+            post_dto = PostDTO(p)
+            posts.append(post_dto)
+
+        posts_json = json.dumps([ob.__dict__ for ob in posts])
+
+        return json.loads(posts_json)
