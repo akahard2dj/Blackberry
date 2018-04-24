468f57f9e56f219c217fd1db75c0bafda56eadc5
akahard2dj <akahard2dj@naver.com>
2018-04-24 14:33:26 +0900

    Create posts.py

---
+++ app/api/v1/posts.py
@@ -0,0 +1,18 @@
+import json
+from flask import g, jsonify, request, current_app, url_for
+
+from app.api.v1_0 import api
+from app.models.post_dao import PostDAO
+
+
+def obj_dict(obj):
+    return obj.__dict__
+
+
+@api.route('/posts/')
+def get_posts():
+    post_dao = PostDAO()
+    posts = post_dao.get_posts()
+    print(json.dumps(posts, default=obj_dict))
+
+    return jsonify({'msg': 'sucess'})
