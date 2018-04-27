36818d219737cb45bc3f0511378795c2375fbf03
akahard2dj <akahard2dj@naver.com>
2018-04-27 11:58:45 +0900

    Update posts.py

--- app/api/v1/posts.py
+++ app/api/v1/posts.py
@@ -1,8 +1,17 @@
-from flask_restful import Resource, fields, marshal_with
+from flask_restful import Resource, fields, marshal_with, reqparse
 from flask import jsonify
 
+from app import db
 from app.models.post_dao import PostDAO
 from app.models.post import Post
+
+post_parser =reqparse.RequestParser()
+post_parser.add_argument(
+    'title', dest='title', type=str
+)
+post_parser.add_argument(
+    'body', dest='body', type=str
+)
 
 post_fields = {
     'id': fields.Integer,
@@ -15,11 +24,19 @@ class PostApi(Resource):
 
     @marshal_with(post_fields)
     def get(self):
-        post_dao = PostDAO()
-        posts = post_dao.get_posts_obj()
-        #posts = Post.query.all()
+        #post_dao = PostDAO()
+        #posts = post_dao.get_posts_obj()
+        posts = Post.query.all()
 
         return posts
 
     def post(self):
-        pass
+        args = post_parser.parse_args()
+        p = Post()
+        p.title = args.title
+        p.body = args.body
+        db.session.add(p)
+        db.session.commit()
+        
+        return jsonify({'msg': 'success'})
+        
