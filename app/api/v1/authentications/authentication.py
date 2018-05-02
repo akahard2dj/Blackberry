3ad64263fc023da97d317defd8e599e500f23edf
akahard2dj <akahard2dj@naver.com>
2018-05-02 16:41:33 +0900

    Timed Token applying

--- app/api/v1/authentications/authentication.py
+++ app/api/v1/authentications/authentication.py
@@ -1,5 +1,6 @@
-from flask import g
+from flask import Blueprint, g
 from flask_httpauth import HTTPBasicAuth
+from flask_restplus import Api
 
 from app.api.v1.users.models import User
 from app.api.v1.authentications.errors import unauthorized
@@ -9,8 +10,11 @@ auth = HTTPBasicAuth()
 
 @auth.verify_password
 def verify_password(email_or_token, password):
-    user = User.query.filter_by(email=email_or_token).first()
+    if password == '':
+        g.current_user = User.verify_auth_token(email_or_token)
+        return g.current_user is not None
 
+    user = User.query.filter_by(email=email_or_token).first()
     if not user or not user.verify_password(password):
         return False
     g.current_user = user
