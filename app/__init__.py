4d2a8b1047244c784aa4aa9053dfed0d8df8243a
akahard2dj <akahard2dj@naver.com>
2018-05-02 16:43:19 +0900

    auth url blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -17,10 +17,12 @@ def create_app(config_name):
     from app.api.v1.articles.views import article_bp
     from app.api.v1.boards.views import board_bp
     from app.api.v1.users.views import user_bp
+    from app.api.v1.authentications.views import auth_bp
     app.register_blueprint(common_bp, url_prefix=url_prefix)
     app.register_blueprint(article_bp, url_prefix=url_prefix + '/articles')
     app.register_blueprint(board_bp, url_prefix=url_prefix + '/boards')
     app.register_blueprint(user_bp, url_prefix=url_prefix + '/users')
+    app.register_blueprint(auth_bp, url_prefix='/auth')
 
     db.init_app(app)
 
