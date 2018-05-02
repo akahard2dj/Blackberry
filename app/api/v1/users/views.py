efed5615dbd6f221d5717253db43bdad47c696a8
akahard2dj <akahard2dj@naver.com>
2018-05-02 15:20:55 +0900

    Update views.py

--- app/api/v1/users/views.py
+++ app/api/v1/users/views.py
@@ -1,39 +1,68 @@
-from flask import Blueprint, request
-from flask_restplus import Api, marshal_with, fields, Resource
+import re
+
+from flask import Blueprint, request, g
+from flask_restplus import Api, marshal_with, fields, Resource, reqparse
 
 from app import db
 from app.api.v1.users.models import User
+
+from app.api.v1.authentications.authentication import auth
+from app.api.v1.authentications.errors import forbidden, unauthorized, bad_request
 
 user_bp = Blueprint('user', __name__)
 api = Api(user_bp)
 
 user_field = {
     'id': fields.Integer,
-    'name': fields.String,
+    'status': fields.String(attribute=lambda x: x.status.name),
+    'username': fields.String,
     'university': fields.String,
     'email': fields.String
 }
 
+user_register_parser = reqparse.RequestParser()
+user_register_parser.add_argument('username')
+user_register_parser.add_argument('email')
+user_register_parser.add_argument('password')
+
 
 @api.route('/<int:user_id>')
 class UserSearchApi(Resource):
+    decorators = [auth.login_required]
 
     @marshal_with(user_field)
     def get(self, user_id):
-        return User.query.filter(User.id == user_id).first()
+        if g.current_user.id == user_id:
+            return User.query.filter(User.id == user_id).first()
+        else:
+            forbidden('Invalid User ID')
 
 
 @api.route('')
 class UserRegistrationApi(Resource):
+    EMAIL_REGEX = re.compile("[^@]+@[^@]+\.[^@]+")
 
+    @marshal_with(user_field)
     def post(self):
-        data = request.json
+        args = user_register_parser.parse_args()
 
-        #TODO: validate data
-        user = User(name=data['name'], university=data['university'],
-                    email=data['email'])
+        # TODO: validate data
+        # Email Validation is confirmed by regex
+        if not self.EMAIL_REGEX.match(args.email):
+            bad_request('Email Validation is failed')
 
+        user = User(username=args.username, password=args.password, email=args.email)
         db.session.add(user)
-        db.session.commit()
 
-        return {"userId": user.id}
+        commit_flag = True
+        try:
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            db.session.flush()
+            commit_flag = False
+
+        if commit_flag:
+            return user
+        else:
+            bad_request('internal server error')
