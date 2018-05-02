48b3ab0a6febbbc233f73c759d45032ae0ebbd24
akahard2dj <akahard2dj@naver.com>
2018-05-02 15:20:35 +0900

    Update models.py

--- app/api/v1/users/models.py
+++ app/api/v1/users/models.py
@@ -1,5 +1,6 @@
 import enum
 from datetime import datetime
+from passlib.hash import pbkdf2_sha256
 
 from app import db
 
@@ -14,9 +15,29 @@ class UserStatus(enum.Enum):
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
+
+    # email is id for login
+    email = db.Column(db.String(128), unique=True)
+    password_hash = db.Column(db.String(128))
     status = db.Column(db.Enum(UserStatus), default='PENDING')
-    name = db.Column(db.Text)
+    username = db.Column(db.Text)
+    # TODO: University Table is needed
     university = db.Column(db.String(128))
-    email = db.Column(db.String(128))
-    created_at = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __init__(self, **kwargs):
+        super(User, self).__init__(**kwargs)
+
+    @property
+    def password(self):
+        raise AttributeError('passowrd is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        custom_pbkdf2 = pbkdf2_sha256.using(rounds=10000)
+        self.password_hash = custom_pbkdf2.hash(password)
+
+    def verify_password(self, password):
+        return pbkdf2_sha256.verify(password, self.password_hash)
