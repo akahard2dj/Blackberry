8eeeacb777a15b362ff36e6a20c7e6a6536e59c2
akahard2dj <akahard2dj@naver.com>
2018-05-03 15:09:01 +0900

    Update views.py
    caching snippets

--- app/api/v1/common/views.py
+++ app/api/v1/common/views.py
@@ -1,4 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+
+from app import cache
 
 common_bp = Blueprint('common', __name__)
 
@@ -6,3 +8,15 @@ common_bp = Blueprint('common', __name__)
 @common_bp.route('/hello')
 def hello():
     return "hello"
+
+
+@common_bp.route('/cache-test')
+def cache_test():
+    rv = cache.get('my-item')
+    print(rv)
+    if rv is None:
+        rv = 100
+        cache.set('my-item', rv, timeout=60)
+        print(rv)
+
+    return jsonify({'value': rv})
