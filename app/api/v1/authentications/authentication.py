4aac8d93310c94eeb7055cd4165b6720a71a351a
akahard2dj <akahard2dj@naver.com>
2018-05-04 11:46:12 +0900

    authentication uses basic and token

--- app/api/v1/authentications/authentication.py
+++ app/api/v1/authentications/authentication.py
@@ -4,36 +4,43 @@ from flask_httpauth import HTTPTokenAuth
 from flask_restplus import Api
 
 from app.api.v1.users.models import User
+from app.api.v1.users.models import UserToken
 from app.api.v1.authentications.errors import unauthorized
 
 auth = HTTPTokenAuth(scheme='Token')
+auth_basic = HTTPBasicAuth()
 
 
 @auth.verify_token
 def verify_token(token):
-    #TODO: AuthenticationToken DB querying
+    # TODO: AuthenticationToken DB querying
     print(token)
-    # TEMP USER
-    g.current_user = User.query.filter(User.id==2).first()
+    user_token = UserToken.query.filter(UserToken.token == token).first()
+    if not user_token:
+        return False
+    else:
+        user = User.query.filter(User.id == user_token.user_id).first()
+        if not user:
+            return False
+    g.current_user = user
     return True
 
-#TODO: HTTPAuth is deprecated.
-'''
-@auth.verify_password
-def verify_password(email_or_token, password):
-    if password == '':
-        g.current_user = User.verify_auth_token(email_or_token)
-        return g.current_user is not None
 
-    user = User.query.filter_by(email=email_or_token).first()
+@auth.error_handler
+def auth_error():
+    return unauthorized('Invalid credentials')
+
+
+@auth_basic.verify_password
+def verify_password(email, password):
+    user = User.query.filter_by(email=email).first()
     if not user or not user.verify_password(password):
         return False
     g.current_user = user
     return True
-'''
 
 
-@auth.error_handler
+@auth_basic.error_handler
 def auth_error():
     return unauthorized('Invalid credentials')
 
