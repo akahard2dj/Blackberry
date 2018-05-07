97241c64639b0f223988e3b2297221effffd127e
Dowon <blessjdw@gmail.com>
2018-05-07 16:26:36 +0900

    users/models.py 리뷰

--- app/api/v1/users/models.py
+++ app/api/v1/users/models.py
@@ -43,33 +43,39 @@ class User(db.Model):
 
     @property
     def password(self):
-        raise AttributeError('passowrd is not a readable attribute')
+        raise AttributeError('password is not a readable attribute')
 
     @password.setter
     def password(self, password):
         custom_pbkdf2 = pbkdf2_sha256.using(rounds=10000)
         self.password_hash = custom_pbkdf2.hash(password)
 
+    # User 모델과는 무관한 기능. 분리하는게 어떨까요?
+    # 이름만으로는 어떤 값을 리턴하는 메소드인지 추측하기 힘드네요.
     def verify_password(self, password):
         return pbkdf2_sha256.verify(password, self.password_hash)
 
+    # User 모델과는 무관한 기능. 분리하는게 어떨까요?
+    # 이름만으로는 어떤 값을 리턴하는 메소드인지 추측하기 힘드네요.
     @staticmethod
     def verify_auth_token(token):
         s = Serializer(current_app.config['SECRET_KEY'])
         try:
             data = s.loads(token)
-        except:
+        except Exception:
             return None
 
         return User.query.get(data['id'])
 
 
+# Token? UserToken? 테이블 이름에서 s를 제외한 이름으로 통일하면 더 좋을 것 같습니다.
 class UserToken(db.Model):
     __tablename__ = 'tokens'
     id = db.Column(db.Integer, primary_key=True)
     token = db.Column(db.String(128), index=True, unique=True, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
     nonce = db.Column(db.String(8), nullable=False)
+    # 컬럼 이름은 동사보다는 명사로 해야 햇갈리지 않더라구요. method는 동사, 컬럼은 명사로 정하면 어떨까요?
     is_issued = db.Column(db.Boolean, default=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow)
@@ -95,6 +101,7 @@ class University(db.Model):
     boards = db.relationship("Board", secondary="university_board_tags")
 
 
+# Tags? 왜 XXXTags인지 주석 추가 좀 부탁 드려요~
 class UniversityBoardTags(db.Model):
     __tablename__ = "university_board_tags"
     id = db.Column(db.Integer, primary_key=True)
