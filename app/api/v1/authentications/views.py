27766e268b9499420df1c5ebadef7e4f80a5fd4d
akahard2dj <akahard2dj@naver.com>
2018-05-18 16:57:41 +0900

    ReqParser 적용

--- app/api/v1/authentications/views.py
+++ app/api/v1/authentications/views.py
@@ -1,7 +1,7 @@
 import re
 
 from flask import g, jsonify, request
-from flask_restplus import Resource
+from flask_restplus import Resource, inputs
 
 from app import cache, get_api
 from app import db
@@ -11,6 +11,7 @@ from app.api.common.utils import random_digit_with_number, random_number
 from app.api.v1.authentications.authentication import auth_basic
 from app.api.v1.authentications.errors import bad_request
 from app.api.v1.common.views import ResponseWrapper
+from app.api.v1.common.exception.exceptions import CommonException
 from app.api.v1.users.models import User, University, UniversityBoardTags, UserToken
 from app.api.v1.boards.models import UserBoardConnector
 
@@ -30,7 +31,7 @@ class TokenApi(Resource):
             try:
                 db.session.commit()
             except Exception as e:
-                bad_request("Internal Error")
+                raise CommonException('Server is not response')
             return ResponseWrapper.ok(data={'token': token.token, 'nonce': token.nonce})
         else:
             return jsonify({'msg': 'Token already has been issued'})
@@ -38,195 +39,180 @@ class TokenApi(Resource):
 
 @api.route('/auth/email-check')
 class EmailCheckApi(Resource):
+    parser = api.parser()
+    parser.add_argument('email', type=inputs.regex("[^@]+@[^@]+\.[^@]+"), required=True)
+    parser.add_argument('uuid', type=str)
 
-    # TODO: WTForm library
-    EMAIL_REGEX = re.compile("[^@]+@[^@]+\.[^@]+")
-
+    @api.expect(parser)
     def get(self):
         # TODO: uuid argument is needed
-        email = request.args.get('email')
-        if not email:
-            bad_request('No email')
+        query_args = self.parser.parse_args()
+        email = query_args['email']
 
-        if not self.EMAIL_REGEX.match(email):
-            bad_request('Email Validation is failed')
-
-        else:
-            # when the information of user is not changed(email, uuid), go to app first
-            from_db_user = User.query.filter(User.email == email).first()
-            if from_db_user is None:
-                # University Checking
-                domain = email.split('@')[-1]
-                university_from_db = University.query.filter(University.domain == domain).first()
-                if university_from_db is None:
-                    return jsonify({'msg': "Requested University is Not Service"})
-                else:
-                    return jsonify({'msg': 'Email Check is completed'})
+        # when the information of user is not changed(email, uuid), go to app first
+        from_db_user = User.query.filter(User.email == email).first()
+        if from_db_user is None:
+            # University Checking
+            domain = email.split('@')[-1]
+            university_from_db = University.query.filter(University.domain == domain).first()
+            if university_from_db is None:
+                return ResponseWrapper.ok('Requested University is Not Service', data={'errors': '{} is not serviced'.format(domain)})
             else:
-                # already registered user
-                # email & password check -> tracing an alteration of phone or re-installed app
-                is_alteration = from_db_user.verify_password('test1234')
-                if not is_alteration:
-                    return jsonify({'msg': 'device is changed'})
-                from_db_user.status = "PENDING"
-                try:
-                    db.session.commit()
-                except Exception as e:
-                    bad_request('internal error')
-                return jsonify({'msg': 'already registered user'})
+                return ResponseWrapper.ok('Email Check is completed')
+        else:
+            # already registered user
+            # email & password check -> tracing an alteration of phone or re-installed app
+            is_alteration = from_db_user.verify_password('test1234')
+            if not is_alteration:
+                return ResponseWrapper.ok('device is changed')
+            from_db_user.status = "PENDING"
+            try:
+                db.session.commit()
+            except Exception as e:
+                CommonException('Server is not response')
+
+            return ResponseWrapper.ok('already registered user')
 
 
 @api.route('/auth/gen-auth-key')
 class GenerateAuthKeyApi(Resource):
-    EMAIL_REGEX = re.compile("[^@]+@[^@]+\.[^@]+")
+    parser = api.parser()
+    parser.add_argument('email', type=inputs.regex("[^@]+@[^@]+\.[^@]+"), required=True)
 
+    @api.expect(parser)
     def get(self):
-        email = request.args.get('email')
-        if not email:
-            bad_request('No email')
+        query_args = self.parser.parse_args()
+        email = query_args['email']
 
-        if not self.EMAIL_REGEX.match(email):
-            bad_request('Email Validation is failed')
+        if cache.get('auth_key:{}'.format(email)):
+            return jsonify({'msg': 'already email is sent'})
+
         else:
-            if cache.get('auth_key:{}'.format(email)):
-                return jsonify({'msg': 'already email is sent'})
-
-            else:
-                auth_key_status = dict()
-                # TODO : auth code is needed to be random function
-                auth_key_status['code'] = random_number()
-                auth_key_status['status'] = 'SENT'
-                # TODO: timeout variable should contain in app config
-                cache.set('auth_key:{}'.format(email), auth_key_status, timeout=600)
-                print('## TEMP LOG: code - {}'.format(auth_key_status['code']), type(auth_key_status['code']))
-                return jsonify({'msg': 'auth key is generated'})
+            auth_key_status = dict()
+            # TODO : auth code is needed to be random function
+            auth_key_status['code'] = random_number()
+            auth_key_status['status'] = 'SENT'
+            # TODO: timeout variable should contain in app config
+            cache.set('auth_key:{}'.format(email), auth_key_status, timeout=600)
+            print('## TEMP LOG: code - {}'.format(auth_key_status['code']), type(auth_key_status['code']))
+            return jsonify({'msg': 'auth key is generated'})
 
 
 @api.route('/auth/confirm-auth-key')
 class ConfirmAuthKeyApi(Resource):
-    EMAIL_REGEX = re.compile("[^@]+@[^@]+\.[^@]+")
+    parser = api.parser()
+    parser.add_argument('email', type=inputs.regex("[^@]+@[^@]+\.[^@]+"), required=True)
+    parser.add_argument('authcode', type=str, required=True)
 
+    @api.expect(parser)
     def get(self):
-        email = request.args.get('email')
-        authcode = request.args.get('authcode')
+        query_args = self.parser.parse_args()
+        email = query_args['email']
+        authcode = query_args['authcode']
 
-        if not email:
-            bad_request('No email')
-
-        if not authcode:
-            bad_request('No authcode')
-
-        if not self.EMAIL_REGEX.match(email):
-            bad_request('Email Validation is failed')
+        res_from_cache = cache.get('auth_key:{}'.format(email))
+        if res_from_cache is None:
+            raise CommonException('Timed out : re-authentication is needed')
         else:
-            res_from_cache = cache.get('auth_key:{}'.format(email))
-            if res_from_cache is None:
-                bad_request('Timed out : re-authentication is needed')
-            else:
-                auth_code_from_cache = res_from_cache['code']
-                if authcode == auth_code_from_cache:
-                    cache.delete('auth_key:{}'.format(email))
-                    from_db_user = User.query.filter(User.email == email).first()
-                    if from_db_user is None:
-                        res_from_cache['status'] = 'CONFIRM'
-                        res_from_cache['usertype'] = 'NEW'
-                        cache.set('auth_key:{}'.format(email), res_from_cache, timeout=1800)
-                        domain = email.split('@')[-1]
-                        university_from_db = University.query.filter(domain == domain).first()
+            auth_code_from_cache = res_from_cache['code']
+            if authcode == auth_code_from_cache:
+                cache.delete('auth_key:{}'.format(email))
+                from_db_user = User.query.filter(User.email == email).first()
+                if from_db_user is None:
+                    res_from_cache['status'] = 'CONFIRM'
+                    res_from_cache['usertype'] = 'NEW'
+                    cache.set('auth_key:{}'.format(email), res_from_cache, timeout=1800)
+                    domain = email.split('@')[-1]
+                    university_from_db = University.query.filter(domain == domain).first()
 
-                        new_user = User()
-                        new_user.email = email
-                        new_user.password = 'test1234'
-                        # TODO: username uniqueness check is needed
-                        new_user.username = random_digit_with_number()
-                        new_user.university = university_from_db.id
+                    new_user = User()
+                    new_user.email = email
+                    new_user.password = 'test1234'
+                    # TODO: username uniqueness check is needed
+                    new_user.username = random_digit_with_number()
+                    new_user.university = university_from_db.id
 
-                        db.session.add(new_user)
-                        try:
-                            db.session.commit()
-                        except Exception as e:
-                            return bad_request('internal error')
-                    else:
-                        res_from_cache['status'] = 'CONFIRM'
-                        res_from_cache['usertype'] = 'OLD'
-                        cache.set('auth_key:{}'.format(email), res_from_cache, timeout=1800)
-
-                    return ResponseWrapper.ok()
+                    db.session.add(new_user)
+                    try:
+                        db.session.commit()
+                    except Exception as e:
+                        return bad_request('internal error')
                 else:
-                    return bad_request('invalid auth_code')
+                    res_from_cache['status'] = 'CONFIRM'
+                    res_from_cache['usertype'] = 'OLD'
+                    cache.set('auth_key:{}'.format(email), res_from_cache, timeout=1800)
+
+                return ResponseWrapper.ok()
+            else:
+                return ResponseWrapper.ok('invalid auth_code', data={'errors': 'auth code check'})
 
 
 @api.route('/auth/registration')
 class RegistrationApi(Resource):
-    EMAIL_REGEX = re.compile("[^@]+@[^@]+\.[^@]+")
+    parser = api.parser()
+    parser.add_argument('email', type=inputs.regex("[^@]+@[^@]+\.[^@]+"), required=True)
+    parser.add_argument('authcode', type=str, required=True)
 
+    @api.expect(parser)
     def get(self):
-        email = request.args.get('email')
-        authcode = request.args.get('authcode')
+        query_args = self.parser.parse_args()
+        email = query_args['email']
+        authcode = query_args['authcode']
 
-        if not email:
-            bad_request('No email')
-
-        if not authcode:
-            bad_request('No authcode')
-
-        if not self.EMAIL_REGEX.match(email):
-            bad_request('Email Validation is failed')
+        res_from_cache = cache.get('auth_key:{}'.format(email))
+        if res_from_cache is None:
+            bad_request('Timed out : re-authentication is needed')
         else:
-            res_from_cache = cache.get('auth_key:{}'.format(email))
-            if res_from_cache is None:
-                bad_request('Timed out : re-authentication is needed')
-            else:
-                auth_code_from_cache = res_from_cache['code']
-                if authcode == auth_code_from_cache:
-                    if res_from_cache['status'] == 'CONFIRM':
-                        if res_from_cache['usertype'] == 'NEW':
-                            to_confirm_user = User.query.filter(User.email == email).first()
-                            to_confirm_user.status = 'USE'
+            auth_code_from_cache = res_from_cache['code']
+            if authcode == auth_code_from_cache:
+                if res_from_cache['status'] == 'CONFIRM':
+                    if res_from_cache['usertype'] == 'NEW':
+                        to_confirm_user = User.query.filter(User.email == email).first()
+                        to_confirm_user.status = 'USE'
 
-                            connector = UserBoardConnector()
-                            connector.user_id = to_confirm_user.id
+                        connector = UserBoardConnector()
+                        connector.user_id = to_confirm_user.id
 
-                            joined_boards = UniversityBoardTags.query.filter(
-                                UniversityBoardTags.university_id == to_confirm_user.university
-                            ).all()
+                        joined_boards = UniversityBoardTags.query.filter(
+                            UniversityBoardTags.university_id == to_confirm_user.university
+                        ).all()
 
-                            for joined_board in joined_boards:
-                                connector.board_id(joined_board.board_id)
+                        for joined_board in joined_boards:
+                            connector.board_id(joined_board.board_id)
 
-                            db.session.add(connector)
+                        db.session.add(connector)
 
+                        token = UserToken()
+                        token.user_id = to_confirm_user.id
+                        token.generate_token()
+                        db.session.add(token)
+
+                        res_from_cache['status'] = 'REGISTER'
+                        cache.set('auth_key:{}'.format(email), res_from_cache, timeout=1800)
+
+                        try:
+                            db.session.commit()
+                        except Exception as e:
+                            return bad_request('internal error')
+                        return ResponseWrapper.ok()
+                    if res_from_cache['usertype'] == 'OLD':
+                        registered_user = User.query.filter(User.email == email).first()
+                        user_token = UserToken.query.filter(UserToken.user_id == registered_user.id).first()
+                        if not user_token:
                             token = UserToken()
-                            token.user_id = to_confirm_user.id
+                            token.user_id = registered_user.id
                             token.generate_token()
                             db.session.add(token)
+                        else:
+                            user_token.generate_token()
 
-                            res_from_cache['status'] = 'REGISTER'
-                            cache.set('auth_key:{}'.format(email), res_from_cache, timeout=1800)
+                        res_from_cache['status'] = 'REGISTER'
+                        cache.set('auth_key:{}'.format(email), res_from_cache, timeout=1800)
 
-                            try:
-                                db.session.commit()
-                            except Exception as e:
-                                return bad_request('internal error')
-                            return ResponseWrapper.ok()
-                        if res_from_cache['usertype'] == 'OLD':
-                            registered_user = User.query.filter(User.email == email).first()
-                            user_token = UserToken.query.filter(UserToken.user_id == registered_user.id).first()
-                            if not user_token:
-                                token = UserToken()
-                                token.user_id = registered_user.id
-                                token.generate_token()
-                                db.session.add(token)
-                            else:
-                                user_token.generate_token()
-
-                            res_from_cache['status'] = 'REGISTER'
-                            cache.set('auth_key:{}'.format(email), res_from_cache, timeout=1800)
-
-                            db.session.commit()
-                            return ResponseWrapper.ok()
-                    else:
-                        bad_request('auth code is not confirmed')
-
+                        db.session.commit()
+                        return ResponseWrapper.ok()
                 else:
-                    return bad_request('invalid auth_code')
+                    bad_request('auth code is not confirmed')
+
+            else:
+                return bad_request('invalid auth_code')
